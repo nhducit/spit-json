7633b10c8c1e6184416f6b0a4cd5d004f58341a7
nhuuduc <nhducit@gmail.com>
2016-01-21 13:36:22 +0700

    init

--- splitJson.js
+++ splitJson.js
@@ -4,24 +4,34 @@ var oboe = require('oboe');
 var mkdirp = require("mkdirp");
 var getDirName = require("path").dirname;
 var program = require('commander');
+var _ = require('lodash');
 //config command line variable
 program
   .version('0.0.1')
   .option('-n, --file-name [type]', 'Input file name')
   .option('-d, --directory [type]', 'Output Directory')
+  .option('-t, --type [type]', 'Input file type, full or short')
+  .option('-ext, --extenion [type]', 'Output file extention')
   .parse(process.argv);
 //
-var outputFolder = program.directory || 'result';
-outputFolder = '/' + outputFolder + '/';
+var outputFolder = '/' + (program.directory || 'result') + '/';
 var currentFolder = __dirname;
 //
 var name = 'response';
 var part = 'part';
 var partCount = 1;
 var responseCount = 1;
-var extension = '.json';
 var outputFileName = '';
+var extension = '.' + (program.extension || 'json');
 var inputFileName = program.fileName || 'data.json';
+var dataPath = {
+full: 'response.customerUsages',
+f: 'response.customerUsages',
+short:  'customerUsages',
+s:  'customerUsages',
+};
+
+var _dataPath = dataPath[program.type] || dataPath.full;
 //
 function splitJson() {
   //create readable stream from file
@@ -29,7 +39,8 @@ function splitJson() {
   //use oboe to parse readable stream
   oboe(readStream)
     .done(function (data) {
-      var customerUsages = data.response.customerUsages;
+
+      var customerUsages = _.get(data, _dataPath);
       var i, length = customerUsages.length;
       partCount = 0;
       for (i = 0; i < length; i++) {
